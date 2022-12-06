2148ea9547cf8338d9ba61a7f6567722abb17961
alexander <avoronin2004@gmail.com>
2022-12-06 11:52:01 +0300

    refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "translate#input"
   get 'translate/output'
+  get 'translate/xml'
+  get 'translate/json'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
